0f857684339aa601505d382d0505f53396843f12
CodeBox123 <99422282+CodeBox123@users.noreply.github.com>
2022-04-14 20:54:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://images.blueskypuppies.store/s_1100x1100,f_avif,quality_60/store_bluesky/c525dc33-876e-4275-803b-1f50a2d456ea-TB-F1B-8787--2-.jpg"];
+var names = ["Family Book","Gramps", "Dad", "Charlie", "Charlotte", "King"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
